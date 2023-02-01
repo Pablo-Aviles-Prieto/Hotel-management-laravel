100312f16f55be91c11343802cc1b56ecd720480
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2023-02-01 23:07:16 +0100

    about-us controller and view working

--- app/Http/Controllers/RoomsController.php
+++ app/Http/Controllers/RoomsController.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Models\Room;
-use Illuminate\Http\Request;
 
 class RoomsController extends Controller
 {
--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\RoomsController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\AboutusController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [RoomsController::class, 'landpage']);
 
 Route::get('/contact', [ContactController::class, 'contactView']);
-Route::post('/contact', [ContactController::class, 'saveForm'])->name('contact.saveForm');;
+Route::post('/contact', [ContactController::class, 'saveForm'])->name('contact.saveForm');
+
+Route::get('/about-us', [AboutusController::class, 'renderView']);
