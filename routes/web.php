d3c3ad2a4f51608942759168531026e65b2c354a
Pablo Avilés <pablo.aviles.prieto@gmail.com>
2023-02-02 00:35:25 +0100

    rooms-grid controller and view working

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\LandpageController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\AboutusController;
 use App\Http\Controllers\OffersController;
+use App\Http\Controllers\RoomGridController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [LandpageController::class, 'renderView']);
@@ -14,3 +15,5 @@ Route::post('/contact', [ContactController::class, 'saveForm'])->name('contact.s
 Route::get('/about-us', [AboutusController::class, 'renderView']);
 
 Route::get('/offers', [OffersController::class, 'renderView']);
+
+Route::get('/rooms-grid', [RoomGridController::class, 'renderRoomGrid']);
